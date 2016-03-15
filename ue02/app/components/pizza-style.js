8dde2ce9bfb839a039fb2f61f9b3e5ae3363e6d0
magdawi <mwimmer.mmt-b2014@fh-salzburg.ac.at>
2016-03-15 14:17:55 +0100

    second assignment

---
+++ ue02/app/components/pizza-style.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'li',
+  click() {
+    //alert("Order se fucking pizza !")
+    var pizza = this.pizza.title
+    var amount = $('#' + pizza)[0].innerHTML
+    $('#' + pizza)[0].innerHTML = parseInt(amount)+1
+  }
+});
